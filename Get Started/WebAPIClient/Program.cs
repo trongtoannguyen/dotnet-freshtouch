b3c296c2285b8d6681658f3495de562fdbe9c8dd
JackieChainnn <trongtoan1609ht@gmail.com>
2023-10-04 23:33:38 +0700

    demo REST API Client

---
+++ Get Started/WebAPIClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+await ProcessRepositoriesAsync(client);
+
+static async Task<List<Repository>> ProcessRepositoriesAsync(HttpClient client)
+{
+    //The endpoint is https://api.github.com/orgs/dotnet/repos.
+    //To retrieve information, it makes an HTTP GET request
+
+    await using Stream stream =
+    await client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+    var repositories =
+        await JsonSerializer.DeserializeAsync<List<Repository>>(stream);
+    return repositories ?? new();
+}
+
+var repositories = await ProcessRepositoriesAsync(client);
+
+foreach (var repo in repositories)
+{
+    Console.WriteLine($"Name: {repo.Name}");
+    Console.WriteLine($"Homepage: {repo.Homepage}");
+    Console.WriteLine($"GitHub: {repo.GitHubHomeUrl}");
+    Console.WriteLine($"Description: {repo.Description}");
+    Console.WriteLine($"Watchers: {repo.Watchers:#,0}");
+    Console.WriteLine($"Last push: {repo.LastPush}");
+    Console.WriteLine();
+}
