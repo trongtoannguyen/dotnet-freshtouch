00c2c1d95501e3fddfd994dcdb35ec1a41ca9f0e
JackieChainnn <trongtoan1609ht@gmail.com>
2023-10-07 01:25:27 +0700

    add field and data

--- Get Started/EfCoreGetStarted/Program.cs
+++ Get Started/EfCoreGetStarted/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EfCoreGetStarted.Models;
+using EfCoreGetStarted.Data;
+
+using HamburgPizzaContext context = new();
+
+var products = context.Products
+    .Where(p => p.Price > 5)
+    .OrderBy(p => p.Name)
+    .ToList();
+
+foreach (var product in products)
+{
+    Console.WriteLine($"Id: {product.Id}");
+    Console.WriteLine($"Name: {product.Name}");
+    Console.WriteLine($"Price: {product.Price}");
+    Console.WriteLine(new string('-', 20));
+}
