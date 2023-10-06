a26c93d26616c0abbd4f4b0f330354dd6bebb9bc
JackieChainnn <trongtoan1609ht@gmail.com>
2023-10-07 00:55:56 +0700

    init Ef Get Started

---
+++ Get Started/EfCoreGetStarted/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace EfCoreGetStarted.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = null!;
+		[Column(TypeName = "decimal(6,2)")]
+		public decimal Price { get; set; }
+	}
+}
+
