fb8532abe320d8008bd8213fb747bdb2fc811907
JackieChainnn <trongtoan1609ht@gmail.com>
2023-10-06 21:53:58 +0700

    Add Validation

--- Get Started/RazorPagesMovie/Models/Movie.cs
+++ Get Started/RazorPagesMovie/Models/Movie.cs
@@ -3,17 +3,31 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models;
 
+/* In this section, validation logic is added to the Movie model.
+The validation rules are enforced any time a user creates or edits a movie. */
 public class Movie
 {
     public int Id { get; set; }
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
     public string Title { get; set; } = string.Empty;
 
-    [Display(Name = "Release Date")]
-    [DataType(DataType.Date)]
+    // [Display(Name = "Release Date")]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
     public DateTime ReleaseDate { get; set; }
-    public string Genre { get; set; } = string.Empty;
 
     [Column(TypeName = "decimal(18, 2)")]
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
     public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [Required]
+    [StringLength(30)]
+    public string Genre { get; set; } = string.Empty;
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    [StringLength(5)]
+    [Required]
     public string Rating { get; set; } = string.Empty;
 }
