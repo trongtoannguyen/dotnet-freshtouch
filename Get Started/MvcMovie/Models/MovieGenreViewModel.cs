3033f355b0952acf5cf59bfef7605cabaaa27d2f
JackieChainnn <trongtoan1609ht@gmail.com>
2023-10-13 01:02:05 +0700

    Mvc Movies add Search

---
+++ Get Started/MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcMovie.Models;
+
+public class MovieGenreViewModel
+{
+    public List<Movie>? Movies { get; set; }
+    // A SelectList containing the list of genres. This allows the user to select a genre from the list.
+    public SelectList? Genres { get; set; }
+    public string? MovieGenre { get; set; }
+    public string? SearchString { get; set; }
+}
