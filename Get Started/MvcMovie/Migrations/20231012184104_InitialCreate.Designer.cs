338ef08374761424ea8bdee507c67e05ba94dc03
JackieChainnn <trongtoan1609ht@gmail.com>
2023-10-13 01:42:39 +0700

    Mvc Movies add new field

--- Get Started/MvcMovie/Migrations/20231012184104_InitialCreate.Designer.cs
+++ Get Started/MvcMovie/Migrations/20231012184104_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using MvcMovie.Data;
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    [Migration("20231011165730_InitialCreate")]
+    [Migration("20231012184104_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
@@ -30,6 +30,9 @@ namespace MvcMovie.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<decimal>("Price")
+                        .HasColumnType("decimal(18, 2)");
+
+                    b.Property<string>("Rating")
                         .HasColumnType("TEXT");
 
                     b.Property<DateTime>("ReleaseDate")
